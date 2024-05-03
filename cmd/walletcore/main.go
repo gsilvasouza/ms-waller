5034067f5b6532432a452919bbe1cd9445e08c94
gsilvasouza <gsilvasouza2000@gmail.com>
2024-05-02 23:29:03 -0300

    feat: kafka

--- cmd/walletcore/main.go
+++ cmd/walletcore/main.go
@@ -5,27 +5,37 @@ import (
 	"database/sql"
 	"fmt"
 
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
 	"github.com/gsilvasouza/ms-waller/internal/database"
 	"github.com/gsilvasouza/ms-waller/internal/event"
+	"github.com/gsilvasouza/ms-waller/internal/event/handler"
 	"github.com/gsilvasouza/ms-waller/internal/usercase/create_account"
 	"github.com/gsilvasouza/ms-waller/internal/usercase/create_client"
 	"github.com/gsilvasouza/ms-waller/internal/usercase/create_transaction"
 	"github.com/gsilvasouza/ms-waller/internal/web"
 	"github.com/gsilvasouza/ms-waller/internal/web/webserver"
 	"github.com/gsilvasouza/ms-waller/pkg/events"
+	"github.com/gsilvasouza/ms-waller/pkg/kafka"
 	"github.com/gsilvasouza/ms-waller/pkg/uow"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", "root", "root", "localhost", "3306", "wallet"))
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", "root", "root", "mysql", "3306", "wallet"))
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 
+	configMap := ckafka.ConfigMap{
+		"bootstrap.servers": "kafka:29092",
+		"group.id":          "wallet",
+	}
+	kafkaProducer := kafka.NewKafkaProducer(&configMap)
+
 	eventDispatcher := events.NewEventDispatcher()
+	eventDispatcher.Register("TransactionCreated", handler.NewTransactionCreatedKafkaHandler(kafkaProducer))
 	transactionCreatedEvent := event.NewTransactionCreated()
 
 	clientDb := database.NewClientDB(db)
@@ -46,7 +56,7 @@ func main() {
 	createAccountUseCase := create_account.NewCreateAccountUseCase(clientDb, accountDb)
 	createClientUseCase := create_client.NewCreateClientUseCase(clientDb)
 
-	webServer := webserver.NewWebServer(":3000")
+	webServer := webserver.NewWebServer(":8080")
 
 	clientHandler := web.NewWebClientHandler(*createClientUseCase)
 	accountHandler := web.NewWebAccountHandler(*createAccountUseCase)
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/gsilvasouza/ms-waller
 go 1.22.2
 
 require (
+	github.com/confluentinc/confluent-kafka-go v1.9.2
 	github.com/google/uuid v1.6.0
 	github.com/stretchr/testify v1.9.0
 )
